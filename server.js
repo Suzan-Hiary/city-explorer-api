8b99564be7ee383a9696cb8722c371b81cc2fa03
Suzan-Hiary <suze-hiary@hotmail.com>
2021-08-12 03:07:54 +0300

    lab09

--- server.js
+++ server.js
@@ -8,41 +8,18 @@ const PORT = process.env.PORT
 const cors = require('cors');
 const weather = require("./data/weather.json");
 app.use(cors());
+const weatherController = require ('./controoler/weather');
+const weatherData = require ('./controoler/weatherData');
+
 
 
 app.get("/", (req, res) => {
     res.send('server Working')
 })
 
-app.get("/weather", (req, res) => {
-    console.log(req.query);
-    res.send(weather);
-})
+app.get("/weather", weatherController ())
 
-app.get("/weather-data", (req, res) => {
-    let lon = Number(req.query['lon']);
-    let lat = Number(req.query['lat']);
-    let searchQuery = '';
-
-    if (req.query['q']) {
-        searchQuery = req.query['q'];
-    }
-    const exploreCity = weather.find((item) => {
-        return (item.city_name.toLowerCase() === searchQuery.toLowerCase() 
-            // item.lat === lat ||
-            // item.lon === lon
-
-        )
-    });
-    console.log(exploreCity)
-
-    const cleandata = []
-    exploreCity.data.forEach(item => {
-        let citydata = new forecast(item);
-        cleandata.push(citydata);
-    })
-    res.send(cleandata);
-})
+app.get("/weather-data",weatherData())
 
 
 
@@ -50,9 +27,3 @@ app.get("/weather-data", (req, res) => {
 app.listen(PORT, () => {
     console.log(`started server on ${PORT}`)
 })
-class forecast {
-    constructor(location) {
-        this.date = location.datetime;
-        this.description = location.weather.description;
-    }
-}
